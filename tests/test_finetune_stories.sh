9c28e970bcb19d68e3a17b899090105fd88fc639
farooqhassaan <hassaanfarooq247@gmail.com>
2020-06-03 12:10:43 +0200

    fine-tuning script and model added

---
+++ tests/test_finetune_stories.sh
@@ -0,0 +1,41 @@
+#!/usr/bin/env bash
+# Test preparation and training on a very small corpus
+# To be run from repo root
+
+set -ev
+
+sp-encode \
+    tests/stories/ \
+    trained_models/de345-root_fine/sp.model \
+    tests/stories-encoded-f
+
+#gpt-2 \
+#    tests/stories-test-run/ \
+#    tests/stories-encoded-f/ \
+#    tests/stories/sp-model.model \
+#    --batch-size 8 \
+#    --g-accum-gradients 1 \
+#    --n-ctx 1024 \
+#    --n-embed 1024 \
+#    --n-hidden 1024 \
+#    --n-head 16 \
+#    --n-layer 24 \
+#    --epochs 50 \
+#    --log-every 2 \
+#    --save-every 50 \
+#    --validate-every 100 \
+#    --clean
+
+# resume training
+gpt-2 \
+   tests/stories-test-run-f/ \
+   tests/stories-encoded-f/ \
+   trained_models/de345-root_fine/sp.model \
+   --batch-size 8 \
+   --g-accum-gradients 1 \
+   --n-ctx 1024 \
+   --n-embed 1024 \
+   --n-hidden 1024 \
+   --n-head 16 \
+   --n-layer 24 \
+   --epochs 2
